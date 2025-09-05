4edf28c1090c366e228d84da38f5a0b09538f774
Juan Pablo Sanchez Zamudio <134655267+juanpisZ@users.noreply.github.com>
2025-09-05 13:16:16 -0500

    Add files via upload

---
+++ Nexus-Frontend/assets/js/componentes.js
@@ -0,0 +1,11 @@
+async function cargarComponentes(id, file) {
+    try {
+        const RESPUESTA = await fetch(file);
+        if (!RESPUESTA.ok) throw new console.error("Error al cargar " + file);
+        const HTML = await RESPUESTA.text();
+        document.getElementById(id).innerHTML = HTML;
+    } catch (error) {
+        console.error(error);
+    }
+
+}
